21c0bdb9d05daea1b1a907702bfc70dcb4f8e682
Astralot8 <82604994+Astralot8@users.noreply.github.com>
2021-11-02 22:00:06 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -48,16 +48,15 @@ public class Main {
 
 
 
-        byte[] example = {72, 101, 108, 108, 111, 33};
-        AsciiCharSequence answer = new AsciiCharSequence(example);
-        System.out.println("Последовательность - " + answer.toString());//Hello!
-        System.out.println("Размер её - " + answer.length());//6
-        System.out.println("Символ под № 1 - " + answer.charAt(1));//e
-        System.out.println("Подпоследовательность - " + answer.subSequence(1, 5));//ello
-//проверка на нарушение инкапсуляции private поля
-        System.out.println(answer.toString());//Hello!
-        example[0] = 74;
-        System.out.println(answer.toString());//Hello!
+        byte[] ex = {72, 101, 108, 108, 111, 33};
+        AsciiCharSequence an = new AsciiCharSequence(ex);
+        System.out.println("Последовательность - " + an.toString());//Hello!
+        System.out.println("Размер её - " + an.length());//6
+        System.out.println("Символ под № 1 - " + an.charAt(1));//e
+        System.out.println("Подпоследовательность - " + an.subSequence(1, 5));//ello
+        System.out.println(an.toString());//Hello!
+        ex[0] = 74;
+        System.out.println(an.toString());//Jello!
     }
 
 }
